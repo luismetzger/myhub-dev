233a26d97002a4f9959eda2f1b8f6379a4329724
Eric Hubbell <eric@myhubapp.com>
2015-06-17 15:12:35 -0600

    Contact Page
    - Rebuilt Form
    - Restyled Page
    - Inserted Mapbox

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -44,16 +44,39 @@ locationHashChanged(); // initial load
 
      // Mapbox - Contact Us
     /* global L */
-    if ($('#map-canvas').length) {
-
     L.mapbox.accessToken = 'pk.eyJ1IjoiZWh1YmJlbGwiLCJhIjoidEdMVWRFZyJ9.o2xk7CcBRujSS-QIheBpwQ';
-    // Create a map in the div #map
-    L.mapbox.map('map-canvas', 'mapbox.emerald')
-    zoomControl: false
-    .setView([39.733, -104.992], 17);
-    }
+    var map = L.mapbox.map('map-canvas', 'mapbox.emerald')
+    .setView([39.733536, -104.992611], 21);
+
+    // Disable drag and zoom handlers.
+    map.scrollWheelZoom.disable();
 
     // Disable tap handler, if present.
     if (map.tap) map.tap.disable();
+
+    L.mapbox.featureLayer({
+    // this feature is in the GeoJSON format: see geojson.org
+    // for the full specification
+    type: 'Feature',
+    geometry: {
+        type: 'Point',
+        // coordinates here are in longitude, latitude order because
+        // x, y is the standard for GeoJSON and many formats
+        coordinates: [
+          -104.992611,
+          39.733536
+        ]
+    },
+    properties: {
+        title: 'Galvanize Golden Triangle',
+        description: '1062 Delaware St. Denver, CO 80202',
+        // one can customize markers by adding simplestyle properties
+        // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+        'marker-size': 'large',
+        'marker-color': '#323a45',
+    }
+    }).addTo(map);
+
+
 
 });
