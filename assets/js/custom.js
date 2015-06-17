1dbb1bbc4a09bc9eae0b2800447900fb20ad60b2
Eric Hubbell <eric@myhubapp.com>
2015-06-17 00:42:55 -0600

    Start of Contact Page
    - Map iFrame
    - Some clean up

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -21,11 +21,6 @@ $(document).ready(function(){
           $(".dropdown").removeClass("open");
       });
 
-
-    // Sticky Kit - Jobs Page Side Nav
-
-    $("#sidebar").stick_in_parent();
-
     // Active URL
 
     function locationHashChanged() {
@@ -38,15 +33,27 @@ locationHashChanged(); // initial load
 
     // Affix Plugin - Jobs Page Side Nav
 
-     $('.jobs-list').affix({
+    if ($('jobs-list').length) {
+    
+    $('.jobs-list').affix({
     offset: {
         top: $('.jobs-list').offset().top
         }
-    });
+    }); 
+    } 
 
+     // Mapbox - Contact Us
+    /* global L */
+    if ($('#map-canvas').length) {
 
+    L.mapbox.accessToken = 'pk.eyJ1IjoiZWh1YmJlbGwiLCJhIjoidEdMVWRFZyJ9.o2xk7CcBRujSS-QIheBpwQ';
+    // Create a map in the div #map
+    L.mapbox.map('map-canvas', 'mapbox.emerald')
+    zoomControl: false
+    .setView([39.733, -104.992], 17);
+    }
 
-
-
+    // Disable tap handler, if present.
+    if (map.tap) map.tap.disable();
 
 });
